f74d6205e5dbd3176d46e9f565b0a92dd34f2ba8
Arthur Baduyen <arthurbaduyen@Arthur.local>
2016-06-22 09:18:45 +0800

    add/delete plugins

--- wp-content/plugins/wp-social-media-slider-lite/admin/views/wpsms-general-instructions.php
+++
@@ -1,9 +0,0 @@
-<div class='wpsms-instructions' >
-
-	<!-- A dummy element for the anchor of our in-page navigation -->
-	<span class='dummy-anchor' id="general-settings" ></span>
-
-	<div class='section-heading' id="general-settings" ><?php echo __( 'General Settings', 'wp-social-media-slider' ); ?> <span> / </span><span><a href="http://wpsocialmediaslider.com/docs/#general-settings" target="_blank" ><?php echo __( 'Documentation', 'wp-social-media-slider' ); ?></a></span></div>
-
-	<br style="clear: both;" />
-</div>
